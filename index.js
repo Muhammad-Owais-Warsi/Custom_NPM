414ef910ca6cd620757349cd371a21a153ed36b7
Owais <warsimuhammadowais@gmail.com>
2024-02-18 15:49:16 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+function mowarsi() {
+    console.log(`
+        █░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█   █▀█ █░█░█ ▄▀█ █ █▀   █▄░█ █▀█ █▀▄▀█
+        ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█   █▄█ ▀▄▀▄▀ █▀█ █ ▄█   █░▀█ █▀▀ █░▀░█
+
+Hey there! 👋 I'm Muhammad Owais Warsi | Open Source Contributor | Much more....
+
+🚀 Let's connect and explore the vast universe of coding together!
+
+   GitHub:   https://github.com/Muhammad-Owais-Warsi
+   LinkedIn: https://www.linkedin.com/in/muhammad-owais-warsi-318987276/
+   Twitter:  https://twitter.com/MO_warsi786
+
+🌟 Excited to embark on coding adventures with you! 🌟
+    `);
+}
+
+mowarsi();
